efc24e7217b8c516ea2c5693b96fd7759ae31968
kenobi.coffeebean <coffeebeankenobi@gmail.com>
2018-11-17 08:41:30 +0000

    char set

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request,[
-            'status' => 'required|max:191',
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
             ]);
       $task = new Task;
@@ -96,7 +96,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'content' => 'required|max:191',
+            'content' => 'required|max:10',
             'status' => 'required|max:191',
         ]);
         
